b919a4f1974d084835ddeb1a9f72375ededfd970
magaiverpr <magaiverpr@gmail.com>
2023-04-26 14:59:39 -0300

    Novos updates
    Adicionado abas, teste de DNS, teste para uma cooperativa específica.

--- precheca.py
+++ precheca.py
@@ -2,7 +2,8 @@ import os
 import platform
 import subprocess
 import socket
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
+import psutil
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QTabWidget
 
 class CheckInfo(QWidget):
     def __init__(self):
@@ -10,30 +11,73 @@ class CheckInfo(QWidget):
         self.initUI()
 
     def initUI(self):
+        # Create the tab widget and tabs
+        tabs = QTabWidget()
+        sys_tab = QWidget()
+        net_tab = QWidget()
+        tabs.addTab(sys_tab, "Principal")
+        tabs.addTab(net_tab, "Network")
+
+    
         # Create labels to display the check results
-        desktop_label = QLabel()
-        user_label = QLabel()
-        ip_label = QLabel()
-        ping_label_central = QLabel()
-        ping_label_confederacao = QLabel()
-        ping_label_internet = QLabel()
+        desktop_label = QLabel(sys_tab)
+        user_label = QLabel(sys_tab)
+        ip_label = QLabel(sys_tab)
+        ping_label_dns_conf = QLabel(sys_tab)
+        ping_label_central = QLabel(sys_tab)
+        ping_label_uad = QLabel(sys_tab)
+        ping_label_confederacao = QLabel(sys_tab)
+        ping_label_internet = QLabel(sys_tab)
+        ips_label = QLabel(net_tab)
+        interface_label = QLabel(net_tab)
+        
         
         # Get the desktop name
         desktop = platform.node()
         desktop_label.setText(f"Nome do computador: {desktop}\n")
 
         # Get the logged in user name
-        user = os.getlogin()
+        user = psutil.Process().username()
         user_label.setText(f"Usuário logado: {user}\n")
 
         # Get the IP address
         hostname = socket.gethostname()
         ip_address = socket.gethostbyname(hostname)
         ip_label.setText(f"IP do computador: {ip_address}\n")
+        
+        
+         # pegar todos os IPs e nomes de interfaces
+        ip_addresses = []
+        interface_names = []
+        for interface, addrs in psutil.net_if_addrs().items():
+            for addr in addrs:
+                if addr.family == socket.AF_INET:
+                    ip_addresses.append(addr.address)
+                    interface_names.append(interface)
+        ips_label.setText(f"IPs do computador:\n {', '.join(ip_addresses)}")
+        ips_label.setWordWrap(True)
+        interface_label.setText(f"Interfaces de rede:\n {', '.join(interface_names)}")
+        interface_label.setWordWrap(True)
+        
+        
+        # Teste de DNS
+        ping_result = subprocess.run(['ping', '-n', '1', 'sisbr.coop.br'], stdout=subprocess.PIPE)
+        if "Resposta de 172.16.1.140: bytes=" in str(ping_result.stdout):
+            ping_label_dns_conf.setText("\n Teste de DNS - Confederação: OK\n")
+            ping_label_dns_conf.setStyleSheet("color: white;"
+                        "background-color: green;"
+                        "selection-color: green;"
+                        "selection-background-color: white;")
+        else:
+            ping_label_dns_conf.setText("\nTeste de DNS - Confederação: FALHA\n")
+            ping_label_dns_conf.setStyleSheet("color: black;"
+                        "background-color: red;"
+                        "selection-color: red;"
+                        "selection-background-color: white;")
 
         # Ping the IP 10.209.4.10 and display the result
         ping_result = subprocess.run(['ping', '-n', '1', '10.209.4.10'], stdout=subprocess.PIPE)
-        if "Resposta de" in str(ping_result.stdout):
+        if "Resposta de 10.209.4.10: bytes=" in str(ping_result.stdout):
             ping_label_central.setText("\n Comunicação com UNICOOB: OK\n")
             ping_label_central.setStyleSheet("color: white;"
                         "background-color: green;"
@@ -62,7 +106,7 @@ class CheckInfo(QWidget):
                         "selection-background-color: white;")
 
          # Ping the IP 131.108.146.241 and display the result
-        ping_result = subprocess.run(['ping', '131.108.146.241'], stdout=subprocess.PIPE)
+        ping_result = subprocess.run(['ping', '-n', '1', '131.108.146.241'], stdout=subprocess.PIPE)
         if "Resposta de" in str(ping_result.stdout):
             ping_label_internet.setText("\nComunicação com Internet: OK\n")
             ping_label_internet.setStyleSheet("color: white;"
@@ -75,25 +119,51 @@ class CheckInfo(QWidget):
                         "background-color: red;"
                         "selection-color: red;"
                         "selection-background-color: white;")
+                        
+        # Ping para UAD
+        ping_result = subprocess.run(['ping', '-n', '1', '10.210.137.246'], stdout=subprocess.PIPE)
+        if "Resposta de" in str(ping_result.stdout):
+            ping_label_uad.setText("\nComunicação com UAD: OK\n")
+            ping_label_uad.setStyleSheet("color: white;"
+                        "background-color: green;"
+                        "selection-color: green;"
+                        "selection-background-color: white;")
+        else:
+            ping_label_uad.setText("\nComunicação com UAD: FALHA\n")
+            ping_label_uad.setStyleSheet("color: black;"
+                        "background-color: red;"
+                        "selection-color: red;"
+                        "selection-background-color: white;")
             
 
             # Create a button to run gpupdate /force
-        button_gpupdate = QPushButton("Atualizar políticas de grupo (gpupdate /force)")
+        button_gpupdate = QPushButton(net_tab)
+        button_gpupdate.setText("Atualizar políticas de grupo (gpupdate /force)")   
         button_gpupdate.clicked.connect(self.run_gpupdate)
 
-        # Add the labels to the layout
-        vbox = QVBoxLayout()
-        vbox.addWidget(desktop_label)
-        vbox.addWidget(user_label)
-        vbox.addWidget(ip_label)
-        vbox.addWidget(ping_label_central)
-        vbox.addWidget(ping_label_confederacao)
-        vbox.addWidget( ping_label_internet)
-        vbox.addWidget(button_gpupdate)
+
+ #aba principal
+        sys_layout = QVBoxLayout(sys_tab)
+        sys_layout.addWidget(desktop_label)
+        sys_layout.addWidget(user_label)
+        sys_layout.addWidget(ip_label)
+        sys_layout.addWidget(ping_label_dns_conf)
+        sys_layout.addWidget(ping_label_central)
+        sys_layout.addWidget(ping_label_uad)
+        sys_layout.addWidget(ping_label_confederacao)
+        sys_layout.addWidget(ping_label_internet)
+        sys_layout.addWidget(button_gpupdate)
+
+#aba Network
+        net_layout = QVBoxLayout(net_tab)
+        net_layout.addWidget(ips_label)
+        net_layout.addWidget(interface_label)
 
         # Set the layout and window properties
+        vbox = QVBoxLayout()
+        vbox.addWidget(tabs)
         self.setLayout(vbox)
-        self.setWindowTitle("Pré-checa Unicoob")
+        self.setWindowTitle("Pré-checa Unicoob - 4351")
         self.setGeometry(100, 100, 300, 150)
         self.show()
         
